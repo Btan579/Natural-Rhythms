4a60ff9030a9f2d905fcac4fd920de073a84452c
Brendan Tanner <btan579@gmail.com>
2018-05-15 22:03:32 -0500

    "Initial app.js with functions"

---
+++ app.js
@@ -0,0 +1,94 @@
+const OPENWEATHERMAP_URL = "api.openweathermap.org";
+const YOUTUBEPLAYLIST_URL = "https://www.googleapis.com/youtube/v3/playlists";
+
+// openweathermap key = 68175e2f52520b2ac4d30af934a37467
+// youtubeplaylist key = AIzaSyDwFW6hwpaeJu8pwYWtoQ7XFndiXYSXFRE
+
+
+const App = {
+    reset: function () {
+            EventListeners.startListeners(); 
+            HTMLRenderer.showIntro(); 
+    },
+
+    search: function(query) {
+        this.getDataFromAPI(query, HTMLRenderer.showDayForecast):
+    },
+
+    getDataFromAPI: function(searchTerm, callback) {
+        let location = searchTerm;
+        const  query = {
+            key: "68175e2f52520b2ac4d30af934a37467",
+            units: "I",
+            city: location
+         };
+         $.getJSON(OPENWEATHERMAP_URL, query, callback).fail(HTMLRenderer.showErr);
+    }        
+    };
+   
+
+
+const EventListeners = {listenersStarted: false,
+    searchQuery: "",
+    
+    startListeners: function () {
+        
+        if (!this.listenersStarted) {
+            this.handleSubmit(); 
+            this.handleHeaderLinkClicked(); 
+            this.handleDayForecastLinkClicked(); 
+            this.listenersStarted = true; 
+        } 
+    },
+    
+    handleHeaderLinkClicked: function () {
+        $(".header__link").click(function (event) {
+            App.reset(); 
+        }); 
+    },
+    
+    handleSubmit: function () {
+        $(".intro__form").submit(function (event) {
+            event.preventDefault(); 
+            const queryTarget = $(this).find(".intro__query"); 
+            this.searchQuery = queryTarget.val(); 
+             // App.search(searchQuery);
+            HTMLRenderer.showDayForecast(); 
+            queryTarget.val(""); 
+        }); 
+    },
+    
+    handleDayForecastLinkClicked: function() {
+        $(".day-forecast__link").click(function (event) {
+            HTMLRenderer.showExtendedForecast(); 
+        }); 
+    } 
+}; 
+
+const HTMLRenderer = {showIntro: function () {
+        $(".intro").removeClass("hidden"); 
+        $(".day-forecast").addClass("hidden"); 
+        $(".extended-forecast").addClass("hidden"); 
+    },
+    
+    showDayForecast: function(data) {
+        $(".intro").addClass("hidden"); 
+        $(".day-forecast").removeClass("hidden"); 
+        $(".extended-forecast").addClass("hidden"); 
+
+        console.log(data);
+    },
+    
+    showExtendedForecast: function () {
+            $(".intro").addClass("hidden"); 
+            $(".day-forecast").addClass("hidden"); 
+            $(".extended-forecast").removeClass("hidden"); 
+
+            alert("showing extened forecast");
+        } ,
+
+        showErr: function(){
+            alert("error");
+        }
+}; 
+$(App.reset())
