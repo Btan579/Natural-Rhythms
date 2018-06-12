0d8226152422964030d374538c8474976a308f79
Brendan Tanner <btan579@gmail.com>
2018-06-12 18:43:56 -0500

    "Commented JS file"

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Javascript globals
 const WEATHERBIT_URL = "https://api.weatherbit.io/v2.0/forecast/daily";
 const YOUTUBEPLAYLIST_URL = "https://www.googleapis.com/youtube/v3/search";
+//  State's varaible help  manage saved  API data and index's for both  API data sets.
 let state = {
     queryResults: {},
     dayIndex: 0,
@@ -12,8 +13,9 @@ let state = {
 // weatherBIT key = 0c56da0e06074738826751b7646a5ebf
 // youtubeplaylist key = AIzaSyDwFW6hwpaeJu8pwYWtoQ7XFndiXYSXFRE
 
-// HTML Rendering
+// HTML Rendering 
 const HTMLRenderer = {
+    // showSection manages each of the sections of html to show and hide when the app is reset.
     showSection: function (sectionToShow) {
         const sections = [".intro", ".playlist", ".day-forecast__results", ".extended-forecast"];
         sections.forEach(function (item, index) {
@@ -21,7 +23,9 @@ const HTMLRenderer = {
         });
         $(sectionToShow).removeClass("hidden");
     },
-
+    //  showDayForecast takes the saved queryResults object based on the city/zip searched and displays the forecast for the day.
+    // Generates each of the daily forecasts using for the extended forcast.
+    // 
     showDayForecast: function (savedData) {
         $(".day-forecast__results").find("*").not(".pEXTforecast").not(".ext-forecast__link").not("br").remove();
         let day = savedData.data[state.dayIndex];
@@ -43,7 +47,7 @@ const HTMLRenderer = {
             let thisDay = savedData.data[i].datetime;
             let dateEXT = new Date(thisDay).toDateString();
 
-        $(".extended-forecast").append(`
+            $(".extended-forecast").append(`
             <div class = "row">
             <div class ="extended-forecast-day">
             <a class="extended-day-link" href="#" data-index=${i}><h4>${dateEXT}</h4></a>
@@ -56,7 +60,7 @@ const HTMLRenderer = {
         }
         App.randomPlaylist();
     },
-
+    //  showPlaylist takes the saved playlist array and displays a single playlist depending on the dayIndex.
     showPlaylist: function (playlists) {
         $("#search-results").empty();
         let currentPlaylist = playlists[state.dayIndex];
@@ -67,7 +71,7 @@ const HTMLRenderer = {
             "</h4></div><div class='col-6'><a target='_blank' href='https://www.youtube.com/playlist?list=" + currentPlaylist[state.playlistIndex].id.playlistId + "'><img class='vidThumbnail' src='" + currentPlaylist[state.playlistIndex].snippet.thumbnails.high.url + "'/></a></div>";
         $("#search-results").append(html);
     },
-
+    // Displays and error alert  if API request fails.
     showErr: function () {
         alert("error");
         return;
@@ -79,7 +83,7 @@ const HTMLRenderer = {
 const EventListeners = {
     listenersStarted: false,
     searchQuery: "",
-
+    // startListeners controls the event listeners to start listening once the App tells startListeners to start.
     startListeners: function () {
         if (!this.listenersStarted) {
             this.handleSubmit();
@@ -89,13 +93,14 @@ const EventListeners = {
             this.listenersStarted = true;
         }
     },
-
+    //  handleHeaderLinkClicked resets the App when the header is clicked
     handleHeaderLinkClicked: function () {
         $(".header__link").click(function (event) {
             App.reset();
         });
     },
-
+    // handleSubmit takes the city or zipcode that is entered and passes it to searchWeather to be used as the query for the API data request..
+    // hides the intro and displays the Day forcast and the suggested playlist.
     handleSubmit: function () {
         $(".intro__form").submit(function (event) {
             event.preventDefault();
@@ -109,7 +114,8 @@ const EventListeners = {
             queryTarget.val("");
         });
     },
-
+    // handleEXTForecastLinkClicked lisened for the extended forecast link to be clicked and hides the day forecast, playlist.
+    // Shows the extended forecast.
     handleEXTForecastLinkClicked: function () {
         $(".ext-forecast__link").on("click", function (event) {
             $(".day-forecast__results").addClass("hidden");
@@ -117,7 +123,7 @@ const EventListeners = {
             $(".playlist").addClass("hidden");
         });
     },
-
+    // handleEXTdaylinkClicked  hides the extended forecast, changes the day forecast to the selected day and shows the new playlist.
     handleEXTdaylinkClicked: function () {
         $(".extended-forecast").on("click", ".extended-day-link", function () {
             event.preventDefault();
@@ -137,6 +143,8 @@ const EventListeners = {
 //  Application Functions
 
 const App = {
+    // reset's all the state variables to empty or 0 to take in new API data. 
+    // Starts event listeners and shows the intro
     reset: function () {
         state = {
             queryResults: {},
@@ -146,11 +154,13 @@ const App = {
         EventListeners.startListeners();
         HTMLRenderer.showSection(".intro");
     },
+    // Accepts the value from the user inputted city or zipcode and passes query to getWeatherDataFromAPI and callback to saveData.
 
     searchWeather: function (query) {
         this.getWeatherDataFromAPI(query, App.saveData);
     },
-
+    //   Request's 6 day forecast from Weatherbit API based on city or zip code.
+    
     getWeatherDataFromAPI: function (searchTerm, callback) {
         const query = {
             key: "0c56da0e06074738826751b7646a5ebf",
@@ -166,7 +176,10 @@ const App = {
         }
         $.getJSON(WEATHERBIT_URL, query, callback).fail(HTMLRenderer.showErr);
     },
-
+    // Saves JSON data from Weatherbit API  in state.queryResults.
+    // Loops over weather description from weather API data and passes to savePlaylistData.
+    // Passes weather data to showDayForecast to be displayed.
+    // Passes weather description concated with songs  to getYoutubePlaylistFromAPI to be used as part of the searchTerm.
     saveData: function (data) {
 
         state.queryResults = data;
@@ -175,7 +188,7 @@ const App = {
         }
         HTMLRenderer.showDayForecast(state.queryResults);
     },
-
+    // getYoutubePlaylistFromAPI makes a API data request to youtube for playlists.
     getYoutubePlaylistFromAPI: function (searchTerm, callback) {
         const query = {
             part: "snippet",
@@ -186,13 +199,13 @@ const App = {
         };
         $.getJSON(YOUTUBEPLAYLIST_URL, query, callback);
     },
-
+    // Saves the YouTube playlist API data in state.playlists and pushes the saved data to be displayed in showPlaylist.
     savePlaylistData: function (data) {
         state.playlists.push(data.items);
         HTMLRenderer.showPlaylist(state.playlists);
 
     },
-
+    // Generates a random playlist to be displayed using the index of the saved playlists data array.
     randomPlaylist: function () {
         state.playlistIndex = Math.floor(Math.random() * 5);
     }
